fe34dc260680364459cbd315d4774582ce2766ef
KyleSantiago09 <santiagoprinzekyle@gmail.com>
2026-01-15 01:02:49 +0800

    First Commit of Portfolio Features: - Bio, About Me, Skills, Description

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    // Find the list of links
+    const menu = document.querySelector(".nav-links");
+    
+    // Toggle the 'active' class on and off
+    menu.classList.toggle("active");
+}
